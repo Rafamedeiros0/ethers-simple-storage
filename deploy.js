dc5a09b68023a6890b5288d306ad8f19b3cf2c04
Rafael Medeiros <32855003+Rafamedeiros0@users.noreply.github.com>
2023-03-09 21:25:54 +0000

    config initial main function

--- deploy.js
+++ deploy.js
@@ -1,3 +1,10 @@
-function hi() {
+async function main() {
   console.log("hi");
 }
+
+main()
+  .then(() => ProcessingInstruction.exit(0))
+  .catch((error) => {
+    console.error(error);
+    // process.exit(1);
+  });
